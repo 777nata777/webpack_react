c7652a6337506628802dc7466d863a5bfa391ac1
777nata777 <m-eme@yandex.ru>
2025-01-29 13:51:14 +0300

    scss  и создается папка в build

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './App.scss';
 
 const App = () => {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(prev => prev + 1)
+    const dicrement = () => setCount(prev => prev - 1)
+
+
     return (
         <div>
-            <h1>Hello1111</h1>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={dicrement}>-</button>
         </div>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 
 
 module.exports = (env) => {
+
     return {
         mode: env.mode ?? 'development',
         entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -14,12 +16,29 @@ module.exports = (env) => {
         },
 
         plugins: [
-            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
-            // new webpack.ProgressPlugin(),
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }), // для генерации HTML-файла
+            new webpack.ProgressPlugin(), // при build видим процент загрузки
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            })
         ],
 
         module: {
             rules: [
+
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        MiniCssExtractPlugin.loader,
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                        ],
+                },
+
                 {
                     test: /\.(js|jsx)$/, // Для файлов .js и .jsx
                     use: {
@@ -34,6 +53,12 @@ module.exports = (env) => {
             extensions: ['.js', '.jsx'], // Автоматически подставлять эти расширения
         },
 
+        devtool: 'inline-source-map', // отслеживаем ошибки
 
+        devServer: {
+            port: 4000,
+            open: true,
+            hot: true,
+        }
     }
 };
