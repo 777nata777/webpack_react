9a8abcc762e59b84cfc55524d2ec7e804f52962f
777nata777 <m-eme@yandex.ru>
2025-02-07 20:17:23 +0300

    исправление страницы о нас

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -7,6 +7,8 @@ import TeamMember3 from '../../assets/about-us/ad0709635b42e822c7aa6ee581776d78.
 import TeamMember4 from '../../assets/about-us/cat-animals-wildlife-fur-baby-animals-whiskers-leopard-Ocelot-savannah-feline-wild-cat-fauna-mammal-1920x1080-px-cat-like-mammal-snout-small-to-medium-sized-cats-carnivoran-organism-bengal-terrestrial-anima.jpg';
 import TeamMember from "@pages/aboutUs/TeamMember";
 
+import ofise1  from '../../assets/about-us/87f483cbc859bdfa73fae4b540288b18.jpg';
+import ofise2  from '../../assets/about-us/87f483cbc859bdfa73fae4b540288b18.jpg';
 const AboutUs = () => {
     return (
         <div className={styles.aboutUs}>
@@ -16,26 +18,26 @@ const AboutUs = () => {
             <section className={styles.team}>
 
                 <TeamMember
-                    name="Член команды 1"
-                    position="CEO"
+                    name="Максим Александрович"
+                    position="Архитектор"
                     imageSrc={TeamMember1}
                     alt="Член команды 1"
                 />
                 <TeamMember
-                    name="Член команды 2"
-                    position="CTO"
+                    name="Наталья Владимировна"
+                    position="Архитектор"
                     imageSrc={TeamMember2}
                     alt="Член команды 2"
                 />
                 <TeamMember
-                    name="Член команды 3"
-                    position="Дизайнер"
+                    name="Мария Николаевна"
+                    position="Архитектор"
                     imageSrc={TeamMember3}
                     alt="Член команды 3"
                 />
                 <TeamMember
-                    name="Член команды 4"
-                    position="Разработчик"
+                    name="Алексей Викторович"
+                    position="Дизайнер"
                     imageSrc={TeamMember4}
                     alt="Член команды 4"
                 />
@@ -45,10 +47,11 @@ const AboutUs = () => {
             <section className={styles.office}>
                 <h2>Наш офис</h2>
                 <div className={styles.officePhoto}>
-                    <img src="/path/to/office1.jpg" alt="Офис 1" />
+                    <img src={ofise1} alt="Офис 1" />
+                    <img src={ofise1} alt="Офис 1" />
                 </div>
                 <div className={styles.officePhoto}>
-                    <img src="/path/to/office2.jpg" alt="Офис 2" />
+                    <img src={ofise2} alt="Офис 2" />
                 </div>
             </section>
         </div>
